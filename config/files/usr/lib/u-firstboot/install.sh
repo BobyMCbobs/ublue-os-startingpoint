c121c231bf6f37832283d53944c725f477192629
Caleb Woodbine <calebwoodbine.public@gmail.com>
2023-12-12 14:51:55 +1300

    chore: resize root filesystem on boot
    resizes btrfs, ext{2,3,4} and xfs

--- config/files/usr/lib/u-firstboot/install.sh
+++ config/files/usr/lib/u-firstboot/install.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
+# BEGIN setup ii user
 useradd -g wheel -u 1000 --create-home ii
 cat <<EOF | chpasswd
 root:ii
 ii:ii
 EOF
+# END setup ii user
+
+# BEGIN grow root disk partition
+ROOT_PART="$(findmnt -n / | awk '{print $2}' | sed 's,/dev/\(.*\)\[.*,\1,g')"
+ROOT_DISK="$(lsblk -J \
+    | jq -r --arg ROOT_PART "$ROOT_PART" '.blockdevices[] | select(.children!=null) | select(.children[].name==$ROOT_PART) | .name')"
+ROOT_DISK_FSTYPE="$(lsblk -fJ \
+    | jq -r --arg ROOT_PART "$ROOT_PART" '.blockdevices[] | select(.children!=null) | .children[] | select(.name==$ROOT_PART) | .fstype')"
+growpart /dev/"$ROOT_DISK" 4 --verbose
+case "$ROOT_DISK_FSTYPE" in
+    btrfs)
+        btrfs filesystem resize max /
+        ;;
+    ext*)
+        resize2fs /dev/"$ROOT_PART"
+        ;;
+    xfs)
+        xfs_growfs /dev/"$ROOT_PART"
+        ;;
+    *)
+        echo "notice: the filesystem type $ROOT_DISK_FSTYPE for the root fs is not plumped in for auto resize"
+        ;;
+esac
+# END grow root disk partition
